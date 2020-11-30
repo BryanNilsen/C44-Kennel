912e7cacbe80e96996506856e0957c2a410d52e6
BryanNilsen <bryannilsen@gmail.com>
2020-11-29 21:37:11 -0600

    employee detail page implemented

--- src/components/employee/EmployeeList.js
+++ src/components/employee/EmployeeList.js
@@ -1,42 +1,31 @@
 import React, { useContext, useEffect } from "react"
-import {LocationContext} from '../location/LocationProvider'
 import { EmployeeContext } from "./EmployeeProvider"
-import { Employee } from "./Employee"
+import { Link } from "react-router-dom"
 import "./Employee.css"
 
-export const EmployeeList = (props) => {
-    // This state changes when `getEmployees()` is invoked below
+export const EmployeeList = props => {
     const { employees, getEmployees } = useContext(EmployeeContext)
-    const { locations, getLocations } = useContext(LocationContext)
 
     useEffect(() => {
-        console.log("EmployeeList: Initial render before data")
-        getLocations()
-        .then(getEmployees)
+        getEmployees()
     }, [])
-
-    /*
-        This effect is solely for learning purposes. The effect
-        it is responding to is that the location state changed.
-    */
-    useEffect(() => {
-        console.log("EmployeeList: Employee state changed")
-        console.log(employees)
-    }, [employees])
 
     return (
         <div className="employees">
             <h1>Employees</h1>
+
             <button onClick={() => props.history.push("/employees/create")}>
                 Add Employee
             </button>
-            <article className="employeeList">
-                {employees.map(employee => {
-                    const loc = locations.find(l => l.id === employee.locationId)
 
-                return <Employee key={employee.id} employee={employee} loc={loc} />
+            <article className="employeeList">
+                {
+                    employees.map(employee => {
+                        return <Link className="employee" key={employee.id} to={`/employees/${employee.id}`}>
+                            <h3>{employee.name}</h3>
+                        </Link>
+                    })
                 }
-                )}
             </article>
         </div>
     )
